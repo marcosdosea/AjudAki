dd72cb4057c0044f87f3db2d79f3982e8e5d9f53
Laila <laila.valenca007@gmail.com>
2024-09-20 13:43:30 -0300

    Projeto reorganizado close #111

---
+++ Codigo/AjudAkiWeb/Core/Areaatuacao.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core;
+
+public partial class Areaatuacao
+{
+    public int Id { get; set; }
+
+    public string Nome { get; set; } = null!;
+
+    public virtual ICollection<Servico> Servicos { get; set; } = new List<Servico>();
+
+    public virtual ICollection<Tiposervico> Tiposervicos { get; set; } = new List<Tiposervico>();
+}
---
+++ Codigo/AjudAkiWeb/Core/Pagamentoassinatura.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Core;
+
+public partial class Pagamentoassinatura
+{
+    public int Id { get; set; }
+
+    public DateTime DataPagamento { get; set; }
+
+    public string NomePlano { get; set; } = null!;
+
+    public string Status { get; set; } = null!;
+
+    public int IdProfissional { get; set; }
+
+    public int IdAssinatura { get; set; }
+
+    public virtual Assinatura IdAssinaturaNavigation { get; set; } = null!;
+
+    public virtual Pessoa IdProfissionalNavigation { get; set; } = null!;
+}
