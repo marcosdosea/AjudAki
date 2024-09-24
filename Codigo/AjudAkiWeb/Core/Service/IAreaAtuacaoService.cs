dc4823b44b64f5f5570be1f294ed4368f397e8b1
marly <marlyssonsd@gmail.com>
2024-09-24 18:27:58 -0300

    Mudança em análise de projeto e criação do arquivo IAreaAtuacaoService.cs

---
+++ Codigo/AjudAkiWeb/Core/Service/IAreaAtuacaoService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IAreaAtuacaoService
+    {
+        uint Create(Areaatuacao areaatuacao);
+        void Edit(Areaatuacao areaatuacao);
+        void Delete(uint idAreaAtuacao);
+        Areaatuacao? Get(uint idAreaAtuacao);
+        IEnumerable<Areaatuacao> GetAll();
+
+    }
+}
