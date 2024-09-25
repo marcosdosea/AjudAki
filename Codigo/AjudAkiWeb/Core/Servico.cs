e460af52ed32a9cfd69814a77403a6f46bb99a6c
Vingreyck <viniciusgreyck@gmail.com>
2024-09-25 00:39:15 -0300

    Feito close#102

--- Codigo/AjudAkiWeb/Core/Servico.cs
+++ Codigo/AjudAkiWeb/Core/Servico.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 
 namespace Core;
-
+    
 public partial class Servico
 {
     public int Id { get; set; }
---
+++ Codigo/AjudAkiWeb/Service/Servico.cs
@@ -0,0 +1,38 @@
+﻿using Core;
+using Core.Service;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+    class Servico : IServico
+    {
+        public uint Create(Pessoa profissional)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(uint id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(Pessoa profissional)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Pessoa? Get(uint id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Pessoa> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
