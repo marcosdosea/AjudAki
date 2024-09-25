012fd54b839c2e915d5666ab46b4582dd6673a04
Vingreyck <viniciusgreyck@gmail.com>
2024-09-25 09:02:38 -0300

    Atualizado a service

--- Codigo/AjudAkiWeb/Service/Servico.cs
+++ Codigo/AjudAkiWeb/Service/Servico.cs
@@ -1,5 +1,6 @@
 ﻿using Core;
 using Core.Service;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,29 +11,67 @@ namespace Service
 {
     class Servico : IServico
     {
+        private readonly AjudakiContext context;
+
+        public Servico(AjudakiContext context)
+        {
+            this.context = context;
+        }
+
+        // <summary>
+        // Criar um novo serviço na base de dados
+        // </summary>
+        // <param name="profissional"></param>
+        // <returns></returns>
         public uint Create(Pessoa profissional)
         {
-            throw new NotImplementedException();
+            context.Add(profissional);
+            context.SaveChanges();
+            return profissional.Id;
         }
 
+        // <summary>
+        // Remover o serviço da base de dados
+        // </summary>
+        // <param name="id"></param>
         public void Delete(uint id)
         {
-            throw new NotImplementedException();
+            var profissional = context.Pessoas.Find(id);
+            if (profissional != null)
+            {
+                context.Remove(profissional);
+                context.SaveChanges();
+            }
         }
 
+        // <summary>
+        // Editar dados do serviço na base de dados
+        // </summary>
+        // <param name="profissional"></param>
         public void Edit(Pessoa profissional)
         {
-            throw new NotImplementedException();
+            context.Update(profissional);   
+            context.SaveChanges();
         }
 
+        // <summary>
+        // Buscar um serviço na base de dados
+        // </summary>
+        // <param name="id"></param>
+        // <returns></returns>
         public Pessoa? Get(uint id)
         {
-            throw new NotImplementedException();
+            return context.Pessoas.Find(id);
         }
 
+        // <summary>
+        // Buscar todos os serviços cadastrados
+        // </summary>
+        // <returns></returns>
         public IEnumerable<Pessoa> GetAll()
         {
-            throw new NotImplementedException();
+            return context.Pessoas.AsNoTracking();
         }
     }
 }
+
