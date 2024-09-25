f1b617701833825dcccd42a77825a540351ea9cf
Vingreyck <viniciusgreyck@gmail.com>
2024-09-25 00:39:15 -0300

    Feito close#102

---
+++ Codigo/AjudAkiWeb/Core/Service/IServico.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IServico
+    {
+        uint Create(Pessoa profissional);
+
+        void Edit(Pessoa profissional);
+
+        void Delete(uint id);
+
+        Pessoa? Get(uint id);
+
+        IEnumerable<Pessoa> GetAll();
+
+    }
+}
