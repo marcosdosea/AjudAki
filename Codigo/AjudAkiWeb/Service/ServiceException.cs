513a9fd2fb82aee75a69fea90a1f758a4a24ffbc
miguelftt <miguelferreirafranca3030@gmail.com>
2024-09-25 21:34:24 -0300

    ajustando erros de conflitos do ServiceException

---
+++ Codigo/AjudAkiWeb/Service/ServiceException.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Service
+{
+    [Serializable]
+    internal class ServiceException : Exception
+    {
+        public ServiceException()
+        {
+        }
+
+        public ServiceException(string? message) : base(message)
+        {
+        }
+
+        public ServiceException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+    }
+}
