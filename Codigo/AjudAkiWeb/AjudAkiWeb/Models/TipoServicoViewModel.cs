68c8d95ca1913848bdcda404630ee8d8faf8d6eb
Vingreyck <viniciusgreyck@gmail.com>
2024-10-03 17:40:45 -0300

    Havia apagado o do ID, agora corrigido, apaguei o do Nome

--- Codigo/AjudAkiWeb/AjudAkiWeb/Models/TipoServicoViewModel.cs
+++ Codigo/AjudAkiWeb/AjudAkiWeb/Models/TipoServicoViewModel.cs
@@ -6,11 +6,11 @@ namespace AjudAkiWeb.Models
 {
     public class TipoServicoViewModel
     {
+        [Display(Name = "Código")]
         [Required(ErrorMessage = "Código do tipo do serviço é obrigatório")]
         [Key]
         public uint Id { get; set; }
 
-        [Display(Name = "Nome")]
         [StringLength(50)]
         public string? Nome { get; set; }
 
