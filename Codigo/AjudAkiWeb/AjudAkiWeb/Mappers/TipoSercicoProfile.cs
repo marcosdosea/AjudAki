9958bf46bb09a041e203005cf780ed0632e96822
Vingreyck <viniciusgreyck@gmail.com>
2024-10-02 21:02:59 -0300

    Implementar CSU011 - Manter tipos de Serviço (2/3)

---
+++ Codigo/AjudAkiWeb/AjudAkiWeb/Mappers/TipoSercicoProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using AjudAkiWeb.Models;
+using Core;
+
+namespace AjudAkiWeb.Mappers
+{
+    public class TipoSercicoProfile : Profile
+    {
+        public TipoSercicoProfile()
+        {
+            CreateMap<TipoServicoViewModel, Tiposervico>().ReverseMap();
+        }
+    }
+}
---
+++ Codigo/AjudAkiWeb/AjudAkiWeb/Models/TipoServicoViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Core;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace AjudAkiWeb.Models
+{
+    public class TipoServicoViewModel
+    {
+        [Display(Name = "Código")]
+        [Required(ErrorMessage = "Código do tipo do serviço é obrigatório")]
+        [Key]
+        public uint Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [StringLength(50)]
+        public string? Nome { get; set; }
+
+        public AssinaturaViewModel? AssinaturaViewModel { get; set; }
+    }
+}
