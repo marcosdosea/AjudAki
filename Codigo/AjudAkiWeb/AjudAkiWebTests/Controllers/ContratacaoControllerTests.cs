ce7475a65fc5cca4d0d6d417af1e02e8ee298dbc
CamilaF56 <camila56@academico.ufs.br>
2024-10-08 18:37:38 -0300

    Implementa CSU01 - Testes do Contratar Serviço
    Close #171

---
+++ Codigo/AjudAkiWeb/AjudAkiWebTests/Controllers/ContratacaoControllerTests.cs
@@ -0,0 +1,216 @@
+﻿using AutoMapper;
+using Moq;
+using Core.Service;
+using AjudAkiWeb.Mappers;
+using Core;
+using Microsoft.AspNetCore.Mvc;
+using AjudAkiWeb.Models;
+
+namespace AjudAkiWeb.Controllers.Tests
+{
+    [TestClass()]
+    public class ContratacaoControllerTests
+    {
+        private static ContratacaoController controller;
+        [TestInitialize]
+        public void Initialize()
+        {
+            // Arrange
+            var mockService = new Mock<IContratacaoService>();
+
+            IMapper mapper = new MapperConfiguration(cfg =>
+                cfg.AddProfile(new ContratacaoProfile())).CreateMapper();
+
+            mockService.Setup(service => service.GetAll())
+                .Returns(GetTestContratacoes());
+            mockService.Setup(service => service.Get(1))
+                .Returns(GetTargetContratacao());
+            mockService.Setup(service => service.Edit(It.IsAny<Contratacao>()))
+                .Verifiable();
+            mockService.Setup(service => service.Create(It.IsAny<Contratacao>()))
+                .Verifiable();
+            controller = new ContratacaoController(mockService.Object, mapper);
+        }
+
+        [TestMethod()]
+        public void IndexTest_Valido()
+        {
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(List<ContratacaoViewModel>));
+
+            List<ContratacaoViewModel>? lista = (List<ContratacaoViewModel>)viewResult.ViewData.Model;
+            Assert.AreEqual(3, lista.Count);
+        }
+
+        [TestMethod()]
+        public void DetailsTest_Valido()
+        {
+            // Act
+            var result = controller.Details(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(ContratacaoViewModel));
+            ContratacaoViewModel contratacaoModel = (ContratacaoViewModel)viewResult.ViewData.Model;
+            Assert.AreEqual("Pintor José Ramos", contratacaoModel.Nome);
+            Assert.AreEqual(DateTime.Parse("2024-02-23"), contratacaoModel.Data);
+        }
+
+        [TestMethod()]
+        public void CreateTest_Get_Valido()
+        {
+            // Act
+            var result = controller.Create();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod()]
+        public void CreateTest_Valido()
+        {
+            // Act
+            var result = controller.Create(GetNewContratacao());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        [TestMethod()]
+        public void CreateTest_Post_Invalido()
+        {
+            // Arrange
+            controller.ModelState.AddModelError("Nome", "Campo requerido");
+
+            // Act
+            var result = controller.Create(GetNewContratacao());
+
+            // Assert
+            Assert.AreEqual(1, controller.ModelState.ErrorCount);
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        [TestMethod()]
+        public void EditTest_Get_Valid()
+        {
+            // Act
+            var result = controller.Edit(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(ContratacaoViewModel));
+            ContratacaoViewModel contratacaoModel = (ContratacaoViewModel)viewResult.ViewData.Model;
+            Assert.AreEqual("Pintor José Ramos", contratacaoModel.Nome);
+            Assert.AreEqual(DateTime.Parse("2024-02-23"), contratacaoModel.Data);
+        }
+
+        [TestMethod()]
+        public void EditTest_Post_Valido()
+        {
+            // Act
+            var result = controller.Edit(GetTargetContratacaoModel().Id, GetTargetContratacaoModel());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        [TestMethod()]
+        public void DeleteTest_Post_Valido()
+        {
+            // Act
+            var result = controller.Delete(1);
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+            ViewResult viewResult = (ViewResult)result;
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(ContratacaoViewModel));
+            ContratacaoViewModel contratacaoModel = (ContratacaoViewModel)viewResult.ViewData.Model;
+            Assert.AreEqual("Pintor José Ramos", contratacaoModel.Nome);
+            Assert.AreEqual(DateTime.Parse("2024-02-23"), contratacaoModel.Data);
+        }
+
+        [TestMethod()]
+        public void DeleteTest_Get_Valid()
+        {
+            // Act
+            var result = controller.Delete(GetTargetContratacaoModel());
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            RedirectToActionResult redirectToActionResult = (RedirectToActionResult)result;
+            Assert.IsNull(redirectToActionResult.ControllerName);
+            Assert.AreEqual("Index", redirectToActionResult.ActionName);
+        }
+
+        private ContratacaoViewModel GetNewContratacao()
+        {
+            return new ContratacaoViewModel
+            {
+                Id = 4,
+                Nome = "Ian Pintor",
+                Data = DateTime.Parse("2024-02-23")
+            };
+        }
+
+        private static Contratacao GetTargetContratacao()
+        {
+            return new Contratacao
+            {
+                Id = 1,
+                Nome = "Pintor José Ramos",
+                Data = DateTime.Parse("2024-02-23")
+            };
+        }
+
+        private ContratacaoViewModel GetTargetContratacaoModel()
+        {
+            return new ContratacaoViewModel
+            {
+                Id = 2,
+                Nome = "Pintor José Ramos",
+                Data = DateTime.Parse("2024-02-23")
+            };
+        }
+
+        private IEnumerable<Contratacao> GetTestContratacoes()
+        {
+            return new List<Contratacao>
+            {
+                new Contratacao
+                {
+                    Id = 1,
+                    Nome = "Pedro do lava-jato",
+                    Data = DateTime.Parse("2024-02-23")
+                },
+                new Contratacao
+                {
+                    Id = 2,
+                    Nome = "Eletricista de filho de Totonho",
+                    Data = DateTime.Parse("2024-02-23")
+                },
+                new Contratacao
+                {
+                    Id = 3,
+                    Nome = "Dósea do fogão",
+                    Data = DateTime.Parse("2024-02-23")
+                },
+            };
+        }
+    }
+}
---
+++ Codigo/AjudAkiWeb/ServiceTests/ContratacaoServiceTests.cs
@@ -0,0 +1,157 @@
+﻿using Core.Service;
+using Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Service.Tests
+{
+    [TestClass()]
+    public class ContratacaoServiceTests
+    {
+        private AjudakiContext context;
+        private IContratacaoService contratacaoService;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            //Arrange
+            var builder = new DbContextOptionsBuilder<AjudakiContext>();
+            builder.UseInMemoryDatabase("Ajudaki");
+            var options = builder.Options;
+
+            context = new AjudakiContext(options);
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            var contratacaos = new List<Contratacao>
+                {
+                    new() { Id = 1,
+                            Nome = "Laila da encanação",
+                            Data = DateTime.Parse("2024-12-31"),
+                            Cep = "12345-678",
+                            Bairro = "Centro",
+                            Rua = "Rua A",
+                            NumResidencia = "100",
+                            PontoReferencia = "Próximo ao mercado",
+                            Status = "Ativo",
+                            IdServico = 1,
+                            IdCliente = 1
+                    },
+                    new() { Id = 2,
+                            Nome = "Dósea do fogão",
+                            Data = DateTime.Parse("2024-11-21"),
+                            Cep = "12345-679",
+                            Bairro = "Bairro B",
+                            Rua = "Rua B",
+                            NumResidencia = "101",
+                            PontoReferencia = "Em frente ao banco",
+                            Status = "Ativo",
+                            IdServico = 2,
+                            IdCliente = 2
+                    },
+                    new() { Id = 3,
+                            Nome = "Mathias eletricista, filho de Tontonho",
+                            Data = DateTime.Parse("2024-11-20"),
+                            Cep = "12345-680",
+                            Bairro = "Bairro C",
+                            Rua = "Rua C",
+                            NumResidencia = "102",
+                            PontoReferencia = "Perto da escola",
+                            Status = "Ativo",
+                            IdServico = 3,
+                            IdCliente = 3},
+                    };
+
+            context.AddRange(contratacaos);
+            context.SaveChanges();
+
+            contratacaoService = new ContratacaoService(context);
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            // Act
+            contratacaoService.Create(new Contratacao()
+            {
+                Id = 4,
+                Nome = "Pedro Pintor",
+                Data = DateTime.Parse("2024-12-25"),
+                Cep = "12345-681",
+                Bairro = "Bairro D",
+                Rua = "Rua D",
+                NumResidencia = "103",
+                PontoReferencia = "Ao lado do posto de gasolina",
+                Status = "Ativo",
+                IdServico = 4,
+                IdCliente = 4
+            });
+
+            // Assert
+            Assert.AreEqual(4, contratacaoService.GetAll().Count());
+            var contratacao = contratacaoService.Get(4);
+            Assert.AreEqual("Pedro Pintor", contratacao.Nome);
+            Assert.AreEqual(DateTime.Parse("2024-12-25"), contratacao.Data);
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            // Act
+            contratacaoService.Delete(2);
+
+            // Assert
+            Assert.AreEqual(2, contratacaoService.GetAll().Count());
+            var autor = contratacaoService.Get(2);
+            Assert.AreEqual(null, autor);
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            //Act 
+            var contratacao = contratacaoService.Get(3);
+            contratacao.Nome = "Damares que arruma casa";
+            contratacao.Data = DateTime.Parse("2024-11-21");
+            contratacao.Cep = "98765-432";
+            contratacao.Bairro = "Novo Bairro";
+            contratacao.Rua = "Nova Rua";
+            contratacao.NumResidencia = "200";
+            contratacao.Status = "Finalizado";
+            contratacaoService.Edit(contratacao);
+
+            //Assert
+            contratacao = contratacaoService.Get(3);
+            Assert.IsNotNull(contratacao);
+            Assert.AreEqual("Damares que arruma casa", contratacao.Nome);
+            Assert.AreEqual(DateTime.Parse("2024-11-21"), contratacao.Data);
+            Assert.AreEqual("98765-432", contratacao.Cep);
+            Assert.AreEqual("Novo Bairro", contratacao.Bairro);
+            Assert.AreEqual("Nova Rua", contratacao.Rua);
+            Assert.AreEqual("200", contratacao.NumResidencia);
+            Assert.AreEqual("Finalizado", contratacao.Status);
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            var contratacao = contratacaoService.Get(2);
+            Assert.IsNotNull(contratacao);
+            Assert.AreEqual("Dósea do fogão", contratacao.Nome);
+            Assert.AreEqual(DateTime.Parse("2024-11-21"), contratacao.Data);
+        }
+
+        [TestMethod()]
+        public void GetAllTest()
+        {
+            // Act
+            var listaContratacao = contratacaoService.GetAll();
+
+            // Assert
+            Assert.IsInstanceOfType(listaContratacao, typeof(IEnumerable<Contratacao>));
+            Assert.IsNotNull(listaContratacao);
+            Assert.AreEqual(3, listaContratacao.Count());
+            Assert.AreEqual((uint)1, listaContratacao.First().Id);
+            Assert.AreEqual("Laila da encanação", listaContratacao.First().Nome);
+        }
+    }
+}
