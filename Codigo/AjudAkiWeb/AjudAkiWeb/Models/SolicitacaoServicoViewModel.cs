9028579a51d7e568394883700fc454a6270b8646
miguelftt <miguelferreirafranca3030@gmail.com>
2024-10-15 21:03:33 -0300

    close #225

--- Codigo/AjudAkiWeb/AjudAkiWeb/Models/SolicitacaoServicoViewModel.cs
+++ Codigo/AjudAkiWeb/AjudAkiWeb/Models/SolicitacaoServicoViewModel.cs
@@ -19,7 +19,7 @@ namespace AjudAkiWeb.Models
 
         [Display(Name = "Data da agenda")]
         [DataType(DataType.Date)]
-        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true, ConvertEmptyStringToNull = true)]
         [Required(ErrorMessage = "Campo obrigatório")]
         public DateTime DataHoraSolicitacao { get; set; }
 
