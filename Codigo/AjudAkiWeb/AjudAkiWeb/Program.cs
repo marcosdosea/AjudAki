a71181e55333b2822aa21ec09e2adcfbe9b6d325
CamilaF56 <camila56@academico.ufs.br>
2024-10-23 23:01:11 -0300

    CSU013 Autenticar Usuário - Implementa Autenticação (Identity)
    - Adiciona configuração de autenticação no projeto.
    - Configura senha e email para autenticação.
    - Adiciona configuração de email no projeto.
    
    Close #239

--- Codigo/AjudAkiWeb/AjudAkiWeb/Program.cs
+++ Codigo/AjudAkiWeb/AjudAkiWeb/Program.cs
@@ -2,6 +2,11 @@ using Core;
 using Core.Service;
 using Microsoft.EntityFrameworkCore;
 using Service;
+using AjudAkiWeb.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using BibliotecaWeb.Helpers;
+using Microsoft.AspNetCore.Identity.UI.Services;
 
 namespace AjudAkiWeb
 {
@@ -10,9 +15,6 @@ namespace AjudAkiWeb
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddDbContext<AjudakiContext>(
-                options => options.UseMySQL(builder.Configuration.GetConnectionString("AjudakiDatabase")));
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
@@ -28,7 +30,55 @@ namespace AjudAkiWeb
             builder.Services.AddTransient<ISolicitacaoServicoService, SolicitacaoServicoService>();
             builder.Services.AddTransient<ITipoServicoService, TipoServicoService>();
 
+            // configuração do envio de emails para o usuário
+            builder.Services.AddTransient<IEmailSender, EmailSender>();
+
             builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            builder.Services.AddDbContext<AjudakiContext>(
+                options => options.UseMySQL(builder.Configuration.GetConnectionString("AjudakiDatabase")));
+
+            builder.Services.AddDbContext<IdentityContext>(
+                options => options.UseMySQL(builder.Configuration.GetConnectionString("IdentityDatabase")));
+
+            builder.Services.AddDefaultIdentity<UsuarioIdentity>(
+                 options =>
+                 {
+                     // SignIn settings
+                     options.SignIn.RequireConfirmedAccount = true;
+                     options.SignIn.RequireConfirmedEmail = true;
+                     options.SignIn.RequireConfirmedPhoneNumber = false;
+
+                     // Password settings
+                     options.Password.RequireDigit = true;
+                     options.Password.RequireLowercase = false;
+                     options.Password.RequireNonAlphanumeric = true;
+                     options.Password.RequireUppercase = false;
+                     options.Password.RequiredLength = 6;
+
+                     // Default User settings.
+                     options.User.AllowedUserNameCharacters =
+                             "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+                     options.User.RequireUniqueEmail = true;
+
+                     // Default Lockout settings
+                     options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                     options.Lockout.MaxFailedAccessAttempts = 5;
+                     options.Lockout.AllowedForNewUsers = true;
+                 }).AddRoles<IdentityRole>()
+                 .AddEntityFrameworkStores<IdentityContext>();
+
+            builder.Services.ConfigureApplicationCookie(options =>
+            {
+                //options.AccessDeniedPath = "/Identity/Autenticar";
+                options.Cookie.Name = "YourAppCookieName";
+                options.Cookie.HttpOnly = true;
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
+                //options.LoginPath = "/Identity/Autenticar";
+                // ReturnUrlParameter requires 
+                options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+                options.SlidingExpiration = true;
+            });
 
             var app = builder.Build();
 
@@ -45,7 +95,10 @@ namespace AjudAkiWeb
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
+
+            app.MapRazorPages();
 
             app.MapControllerRoute(
                 name: "default",
