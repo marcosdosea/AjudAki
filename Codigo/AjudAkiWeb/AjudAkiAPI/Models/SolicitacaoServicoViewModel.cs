d61f9c7b062032a4d2a115aa8bb5a6f768c79915
marly <marlyssonsd@gmail.com>
2024-10-28 20:42:38 -0300

    *Criação e configuração do ProjetoAPI*

---
+++ Codigo/AjudAkiWeb/AjudAkiAPI/Models/SolicitacaoServicoViewModel.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace AjudAkiAPI.Models
+{
+    public enum StatusSolicitacaoEnum
+    {
+        RECUSADO, ACEITO, PENDENTE, FINALIZADO
+    }
+
+    public class SolicitacaoServicoViewModel
+    {
+        [Display(Name = "Código da solicitação de serviço")]
+        [Required(ErrorMessage = "Código da solicitação de serviço é obrigatório")]
+        [Key]
+        public uint Id { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(50)]
+        public string Nome { get; set; } = null!;
+
+        [Display(Name = "Data da Solicitação")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true, ConvertEmptyStringToNull = true)]
+        [Required(ErrorMessage = "Campo obrigatório")]
+        public DateTime DataHoraSolicitacao { get; set; }
+
+        [Display(Name = "Status:  RECUSADO, ACEITO, PENDENTE, FINALIZADO")]
+        public StatusSolicitacaoEnum? Status { get; set; } = null;
+
+        [Range(0.0, double.MaxValue, ErrorMessage = "O valor deve ser positivo.")]
+        public decimal? Valor { get; set; }
+
+        [Required(ErrorMessage = "Campo obrigatório")]
+        [StringLength(300)]
+        public string? Descricao { get; set; }
+
+        [Display(Name = "Código do cliente")]
+        [Required(ErrorMessage = "Código do cliente é obrigatório")]
+        [Key]
+        public uint IdCliente { get; set; }
+
+        [Display(Name = "Código do profisisonal")]
+        [Required(ErrorMessage = "Código do profisisonal é obrigatório")]
+        [Key]
+        public uint IdProfissional { get; set; }
+
+        [Display(Name = "Código do tipo de serviço")]
+        [Required(ErrorMessage = "Código do tipo de serviço é obrigatório")]
+        [Key]
+        public uint IdTipoServico { get; set; }
+
+        public SelectList? ListaClientes { get; set; }
+        public SelectList? ListaProfissionais { get; set; }
+        public SelectList? ListaTiposServico { get; set; }
+    }
+}
