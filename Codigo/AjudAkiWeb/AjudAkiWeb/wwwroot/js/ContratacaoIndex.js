185a7a411512ddb9ec84f3b0c46ecf08be995ec4
miguelftt <miguelferreirafranca3030@gmail.com>
2024-10-30 20:00:38 -0300

    close #281

---
+++ Codigo/AjudAkiWeb/AjudAkiWeb/wwwroot/js/ContratacaoIndex.js
@@ -0,0 +1,18 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const contratacaoCards = document.querySelectorAll(".contratacao-card");
+
+    contratacaoCards.forEach(card => {
+        const status = card.getAttribute("data-status");
+        const statusMessage = card.querySelector(".status-mensagem");
+        switch (status) {
+            case "Aceito":
+                statusMessage.innerHTML = 'Status: <strong style="color: green;">Serviço aceito</strong>';
+                break;
+            case "Recusado":
+                statusMessage.innerHTML = 'Status: <strong style="color: red;">Serviço recusado</strong>';
+                break;
+            default:
+                statusMessage.innerHTML = 'Status: <strong style="color: #ff9900;">Aguardando a confirmação da visita</strong>';
+        }
+    });
+});
